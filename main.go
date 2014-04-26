73a3aa1b296b5c23985a41c02bc891ffcba5c097
Dan Ott <thedanielott@gmail.com>
2014-04-26 13:30:24 -0600

    FAST

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 	"net/http"
 
 	"github.com/codegangsta/martini"
@@ -11,12 +12,30 @@ import (
 
 var apiKey = "IP"
 
+type queryResult struct {
+	passage Passage
+	err     error
+}
+
+type simpleStore map[string]queryResult
+
+// DB Returns a martini.Handler
+func DB() martini.Handler {
+	db := make(simpleStore)
+
+	return func(c martini.Context) {
+		c.Map(db)
+		c.Next()
+	}
+}
 func main() {
 	flag.StringVar(&apiKey, "api-key", "IP", "API Key for the ESV API")
 	envflag.Parse()
 
 	m := martini.Classic()
 	m.Use(render.Renderer())
+	m.Use(DB())
+
 	m.Get("/search", PassageQueryHandler)
 	m.Get("/", HelpHandler)
 	m.Run()
@@ -24,13 +43,24 @@ func main() {
 
 type arbitraryJSON map[string]interface{}
 
-func PassageQueryHandler(ren render.Render, req *http.Request) {
-	passageQuery, err := PassageQuery(req.URL.Query().Get("q"))
+func PassageQueryHandler(ren render.Render, req *http.Request, store simpleStore) {
+	var passage Passage
+	var err error
+
+	q := req.URL.Query().Get("q")
+	if v, ok := store[q]; ok {
+		log.Printf("Found in cache")
+		passage, err = v.passage, v.err
+	} else {
+		log.Printf("Found in api")
+		passage, err = PassageQuery(q)
+		store[q] = queryResult{passage, err}
+	}
 
 	if err != nil {
 		ren.JSON(404, arbitraryJSON{"status": 404, "message": err.Error()})
 	} else {
-		ren.JSON(200, passageQuery)
+		ren.JSON(200, passage)
 	}
 }
 
